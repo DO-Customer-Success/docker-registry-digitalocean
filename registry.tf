badfcd84aeb2888457822b5d523aa1c995bcd576
Joseph D. Marhee <jmarhee@interiorae.com>
2016-10-03 11:19:07 -0500

    added block storage volume creation/mount for registry data

--- registry.tf
+++ registry.tf
@@ -44,11 +44,13 @@ resource "digitalocean_droplet" "registry" {
 
     provisioner "remote-exec" {
       inline = [
+                "mkfs.ext3 -F /dev/sda",
+                "mkdir /registry-data && mount -t ext3 /dev/sda /registry-data && mkdir /registry-data/registry",
                 "echo `docker run --entrypoint htpasswd registry:2 -Bbn imgadm ${file("registry_auth")}` > auth/htpasswd",
                 "cd certs && openssl genrsa -out registry.key 2048",
                 "openssl req -subj \"/C=US/ST=NY/L=Flavortown/O=Guy Fieri/OU=Development Registry/CN=${var.common_name}\" -new -key registry.key -out registry.csr",
                 "openssl x509 -req -in registry.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out registry.crt -days 500 -sha256",
-                "docker run -d -p 5000:5000 --restart=always --name registry -v /registry-data:/var/lib/registry -v /root/certs:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key -e \"REGISTRY_AUTH=htpasswd\" -e \"REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm\" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /root/auth:/auth registry:2"
+                "docker run -d -p 5000:5000 --restart=always --name registry -v /registry-data/registry:/var/lib/registry -v /root/certs:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key -e \"REGISTRY_AUTH=htpasswd\" -e \"REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm\" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /root/auth:/auth registry:2"
                 ]
     }
 }
